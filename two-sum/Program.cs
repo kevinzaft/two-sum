eef16ec5d1cc9b7128e587abc539f70dbf99d044
Kevin Huangfu <kevinzaft@gmail.com>
2019-02-03 21:10:04 -0500

    first commit

--- two-sum/Program.cs
+++ two-sum/Program.cs
@@ -8,6 +8,42 @@ namespace two_sum
 {
     class Program
     {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, List<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    dic[nums[i]].Add(i);
+                }
+                else
+                {
+                    dic.Add(nums[i], new List<int>() { i });
+                }
+
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var dif = target - nums[i];
+                try
+                {
+                    if (dic[dif].First() != i)
+                    {
+                        return new int[] { i, dic[dif].First() };
+                    }
+                    else if (dic[dif].Count() > 1)
+                    {
+                        return new int[] { i, dic[dif][1] };
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            return new int[] { -1, -1 };
+        }
         static void Main(string[] args)
         {
         }
